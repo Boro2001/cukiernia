acb5090d96e973f533d54e153d02562dc528d989
Mikołaj Borowicz <62395530+Boro2001@users.noreply.github.com>
2022-02-02 11:53:24 +0100

    nie weim czy to działa

--- src/main/java/sweet/dreams/com/App.java
+++ src/main/java/sweet/dreams/com/App.java
@@ -1,9 +1,31 @@
 package sweet.dreams.com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String url = "jdbc:postgresql://localhost:5432/cukiernia";
+        String user = "admin_cukiernia";
+        String password = "1234*";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT VERSION()")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
+
